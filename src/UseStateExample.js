bc286057c8a3de9ffe35ccd1eab8ac7127e63c3e
smyye <smyyeoztrk43@gmail.com>
2024-10-07 15:07:45 +0300

    started

---
+++ src/UseStateExample.js
@@ -0,0 +1,46 @@
+import { useReducer, useState } from "react";
+import { reducer, initialState } from "./reducer";
+
+const UseStateExample = () => {
+  const [catImage, setCatImage] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getCatImage = async () => {
+    const url = "https://api.thecatapi.com/v1/images/search";
+
+    // setLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setCatImage(data[0].url);
+      setError("");
+    } catch (error) {
+      setError("DATA CAN NOT BE FETCHED");
+      setCatImage("");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  console.log(error);
+  console.log(catImage);
+
+  return (
+    <div>
+      <button
+        onClick={getCatImage}
+        disabled={loading}
+        style={{ display: "block", margin: "1rem" }}
+      >
+        Get Cat Image
+      </button>
+      {error && <h2>{error}</h2>}
+      {catImage && <img width="50%" src={catImage} alt="img" />}
+    </div>
+  );
+};
+
+export default UseStateExample;
---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+  loading: false,
+  catImage: "",
+  error: "",
+};
+
+export const reducer = (initialState, action) => {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
